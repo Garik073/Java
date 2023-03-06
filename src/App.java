b9c57f043437859225f7bc8ab0ffe0223fd68756
Garik073 <gariklukin073@gmail.com>
2023-03-07 00:31:54 +0300

    HomeWork1.2

--- src/App.java
+++ src/App.java
@@ -1,12 +1,10 @@
-import java.lang.management.ThreadInfo;
 import java.util.Scanner;
 
 public class App {
-    /**
-     * @param args
-     */
+   
     public static void  main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         System.out.println("Введите первое число: " );
         int a = in.nextInt();
         System.out.println("Введите второе число: " );
@@ -17,22 +15,43 @@ public class App {
         int summ = a + b;
         System.out.println("Сумма введенных чисел: "+ summ);
 
-        Sravnenie(a, b, summ);
+        Sravnenie(summ);
+        isPositive(summ);
    
     }
-    private static  boolean  Sravnenie(int a, int b, int summ) {
+    private static  boolean  Sravnenie (int summ) {
+        Scanner in = new Scanner(System.in);
+    
         // проверить, что сумма a и b лежит между 10 и 20
         if (summ >= 10 && summ <= 20) {
             System.out.println("Сумма введенных чисел в диопазоне от 10 до 20 ");
-            System.out.println(true);
             return true;
         }
         else{
-            System.out.println("Сумма введенных чисел привышает диапозон ");
+            System.out.println("Сумма введенных чисел не входит в диапазон ");
             return false;
-
-            }
-
-
+        }
+        
+        
+        
     }
+    
+
+    private static  void isPositive(int summ) {
+        
+        
+        // проверить, что х положительное
+        if (summ > 0){
+            System.out.println("Число положительное");  
+            
+        }
+        else if (summ < 0){
+            System.out.println("Число отрицательное");
+        }
+        else {
+            System.out.println("Число равно 0");
+            
+        }
+    }
+    
 }
